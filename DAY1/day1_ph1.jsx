0cd7a79240a2a076c56365912e5d38d28f8fe198
shreeee04 <727723eucs225@skcet.ac.in>
2025-02-19 18:05:35 +0530

    Delete DAY1 directory

--- DAY1/day1_ph1.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const DAY1PH1 =()=>{
-    return(
-        <div>
-            <p>This is Team Functional Component</p>
-        </div>
-    );
-};
-export default DAY1PH1;
